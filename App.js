509345fc95b636180868f3748d1b1cb056467af7
baisilg <pgbaisil@gmail.com>
2020-07-17 18:17:52 +0530

    safe area

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'blanchedalmond',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
