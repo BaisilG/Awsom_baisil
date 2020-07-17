f35865d0e99a3c395c8b4288bdd4a2ade69834c0
baisilg <pgbaisil@gmail.com>
2020-07-17 16:54:16 +0530

    react-update

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome this is me Baisil</Text>
+      <Text>Praise to God</Text>
+
       <StatusBar style="auto" />
     </View>
   );
