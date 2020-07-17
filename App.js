ff323d0e9e72ec745bce86da4dc80633dae9712d
baisilg <pgbaisil@gmail.com>
2020-07-17 18:40:42 +0530

    update

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View , image, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View , Image, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Praise to God</Text>
-      <Image source={require("./assets/icon.png")}/>
+
+      {/* <Image source={require("./assets/icon.png")}/> */}
+
+      <Image source={{
+        Text : 'my name is baisil',
+        width: 100,
+        height : '100',
+        uri: 'https://reactnative.dev/img/tiny_logo.png',}} />
       
       <StatusBar style="auto" />
     </SafeAreaView>
