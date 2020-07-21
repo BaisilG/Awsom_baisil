9c5b5d2ed188558e32996d11cdd580ec66d9487b
baisilg <pgbaisil@gmail.com>
2020-07-21 10:47:42 +0530

    clean code

--- App.js
+++ App.js
@@ -7,18 +7,7 @@ export default function App() {
 
 
     <SafeAreaView style={styles.container}>
-      <Text>Praise to God</Text>
-      {/* <Image source={require("./assets/icon.png")}/> */}
-     <Image 
-
-      blurRadius = {0.1}
-      fadeDuration = {4000}
-      source={{        
-        width: 100,
-        height : 100,
-        uri: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U',}} />
       
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
