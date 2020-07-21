5a2cd466145642b1d13330a3a25f1a09af669c5a
baisilg <pgbaisil@gmail.com>
2020-07-21 10:51:17 +0530

    Button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View , Image, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View , Image, SafeAreaView, Button} from 'react-native';
 
 export default function App() {
   return (
@@ -8,6 +8,8 @@ export default function App() {
 
     <SafeAreaView style={styles.container}>
       
+      <Button title="Click Me" onPress = {() => console.log ("Button taped")}/>
+
     </SafeAreaView>
   );
 }
